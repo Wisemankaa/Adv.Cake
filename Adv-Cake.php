4024e31d43434b6712c159676e8838d1ef2ed630
Kyrov Andrew <ka@censit.ru>
2021-09-02 21:28:09 +0500

    third commit

--- Adv-Cake.php
+++ Adv-Cake.php
@@ -6,29 +6,33 @@
 <body>
 <?php
 
+echo revertCharacters("Привет! Давно не виделись.") . '<BR />';
+echo revertCharacters("Привет! ДавНо не виде-лись.") . '<BR />';
+echo revertCharacters("Проверка, киРИлицы! как-То тАк.") . '<BR />';
 
-echo revertCharacters("Привет! Давно не виделись.");
-
-function revertCharacters($incomeStr) {
-  $arrWords = explode(" ", $incomeStr);
-  $arrReversWords = [];
-  foreach ($arrWords as $word) {
-    $arrReversWords[] = my_strrev($word);
+function revertCharacters (string $incstr) : string {
+  $res = preg_split('/\W/u', $incstr, -1, PREG_SPLIT_OFFSET_CAPTURE);
+  $resstr = '';
+  foreach ($res as $value) {
+    if ($value[1] == 0) {
+      $resstr = my_strrev($value[0]);
+    } else {
+      $resstr = $resstr . $incstr[$value[1]-1] . my_strrev($value[0]);
+    }
   }
-  // return mb_strrev($incstr);
-  return implode(" ", $arrReversWords);
-};
+  return $resstr;
+}
 
 function my_strrev(string $incomeStr, string $encoding = null): string
 {
-    $res = preg_match('/([^!?.,;:]*)([!?.,;:])/',$incomeStr,$matches);
-    if ($res) {
-      $chars = array_reverse(mb_str_split($matches[1], 1, $encoding ?: mb_internal_encoding()));
-      $chars[] = $matches[2];
-    } else {
-      $chars = array_reverse(mb_str_split($incomeStr, 1, $encoding ?: mb_internal_encoding()));
+  $chars_i = mb_str_split($incomeStr, 1, $encoding ?: mb_internal_encoding());
+  $chars = array_reverse(mb_str_split(mb_strtolower($incomeStr), 1, $encoding ?: mb_internal_encoding()));
+  for ($i = 0; $i < count($chars); $i++) {
+    if (mb_strtolower($chars_i[$i]) != $chars_i[$i]) {
+      $chars[$i] = mb_strtoupper($chars[$i]);
     }
-    return implode('', $chars);
+  }
+  return implode('', $chars);
 }
 
 ?>
